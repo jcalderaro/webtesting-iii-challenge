667a5c07054e70453286c5bee78ba75e10732414
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-11 15:09:35 -0800

    Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+// -------------------------------------------------------------------------------------- //
+
+// ### Dashboard
+
+// - shows the controls and display
+
+// -------------------------------------------------------------------------------------- //
+
+import React from "react";
+import {render, fireEvent } from "@testing-library/react";
+import Dashboard from "../dashboard/Dashboard";
+
+// -------------------------------------------------------------------------------------- //
+
+test("Dashboard renders correctly", () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+// -------------------------------------------------------------------------------------- //
