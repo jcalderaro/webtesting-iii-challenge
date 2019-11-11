e9c05624239a7b76ab0d88e6f288c3ee7109f59a
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-11 15:12:18 -0800

    Controls

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,18 +1,33 @@
+// -------------------------------------------------------------------------------------- //
+
 import React from 'react';
+
+// -------------------------------------------------------------------------------------- //
 
 const Controls = props => {
   const { locked, closed, toggleLocked, toggleClosed } = props;
 
   return (
     <div className="controls panel">
-      <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
+      <button data-testid="toggle-locked" disabled={!closed} onClick={toggleLocked} className="toggle-btn">
         {locked ? 'Unlock Gate' : 'Lock Gate'}
       </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
+      <button data-testid="toggle-closed" disabled={locked} onClick={toggleClosed} className="toggle-btn">
         {closed ? 'Open Gate' : 'Close Gate'}
       </button>
     </div>
   );
 };
 
+// -------------------------------------------------------------------------------------- //
+
 export default Controls;
+
+// -------------------------------------------------------------------------------------- // 
+/* Changes
+
+line 12: <button disabled={!closed} onClick={toggleLocked} className="toggle-btn"></button>
+
+line 15: <button disabled={locked} onClick={toggleClosed} className="toggle-btn"> 
+
+*/// -------------------------------------------------------------------------------------- //
