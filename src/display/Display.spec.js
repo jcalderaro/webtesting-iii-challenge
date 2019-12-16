ff3c6dd26e07149ab355356693f1e818afe843e8
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-12-16 15:40:11 -0800

    Display Tests Done

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,62 @@
-// Test away!
+// -------------------------------------------------------------------------------------- //
+
+// ### Display Component
+// - displays if gate is open/closed and if it is locked/unlocked
+
+// - displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise
+
+// - displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise
+// - when `locked` or `closed` use the `red-led` class
+// - when `unlocked` or `open` use the `green-led` class
+
+// -------------------------------------------------------------------------------------- //
+
+import React from "react";
+import {render, fireEvent } from "@testing-library/react";
+import Display from "../display/Display";
+
+// -------------------------------------------------------------------------------------- //
+
+test("Display renders correctly", () => {
+    expect(render(<Display />)).toMatchSnapshot();
+});
+
+// -------------------------------------------------------------------------------------- //
+
+test("displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise", () => {
+    const { getByText } = render(
+        <Display 
+        closed={true}
+        />
+    )
+    expect(getByText(/Closed/i));
+})
+
+// -------------------------------------------------------------------------------------- //
+
+test("displays 'Open' if the `closed` prop is `true` and 'Open' if otherwise", () => {
+    const { getByText } = render(
+        <Display 
+        closed={false}
+        />
+    )
+    expect(getByText(/Open/i));
+})
+
+// -------------------------------------------------------------------------------------- //
+
+test("when `locked` or `closed` use the `red-led` class", () => {
+    
+    const { getByText } = render(
+        <Display 
+        locked={true}
+        closed={true}
+        />
+    )
+    const lockedDiv = getByText('Locked')
+    const closedDiv = getByText('Closed')
+    expect(lockedDiv.className).toBe('led red-led')
+    expect(closedDiv.className).toBe('led red-led')
+})
+
+// -------------------------------------------------------------------------------------- //
